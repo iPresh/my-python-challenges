2590e0d733121947ba275be956d94ef84c24dada
iPresh <amuhprecious@gmail.com>
2023-11-19 01:10:20 +1200

    for loop practice

---
+++ for-loop/README.md
@@ -0,0 +1,2 @@
+### Instruction
+Write a program that prints the numbers from 1 to 100, noting the multiples of 3 and 5.
---
+++ for-loop/main.py
@@ -0,0 +1,9 @@
+for i in range(1,101):
+  if (i%3 == 0) and (i%5 == 0):
+    print(i, "is a multiple of 3 and 5")
+  elif (i%3 == 0):  
+    print(i, "is a multiple of 3")
+  elif (i%5 == 0):  
+    print(i, "is a multiple of 5")
+  else:
+    print(i)
